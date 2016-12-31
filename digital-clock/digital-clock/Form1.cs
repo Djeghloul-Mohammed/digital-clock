ab471f140eda931175af0efb8b8fb8a17badd976
Mohammed Djeghloul <tedresso@gmail.com>
2016-12-31 14:47:28 +0100

    finishing code

--- digital-clock/digital-clock/Form1.cs
+++ digital-clock/digital-clock/Form1.cs
@@ -21,6 +21,41 @@ namespace digital_clock
         private void Form1_Load(object sender, EventArgs e)
         {
             t.Interval = 1000;
+            t.Tick += new EventHandler(this.t_tick);
+            t.Start();
+
         }
+        private void t_tick(object sender,EventArgs e)
+        {
+
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+
+            string time = "";
+            if(hh< 10)
+            {
+                time += "0 " + hh;
+            }
+            else
+            {
+                time += hh;            }
+            time += ":";
+            if(mm<10)
+            {
+                time += "0" + mm;
+            }
+            else { time += mm; }
+            time += ":";
+            if(ss<10)
+
+            {
+                time += "0 " + ss;
+            }
+            else { time += ss; }
+            label1.Text = time;
+        }
+       
+
     }
 }
